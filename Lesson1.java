d339dc604d253cd6bb9388911e823e15a4435459
cocoon1 <1179148720@qq.com>
2019-04-07 11:32:48 +0800

    first_java

---
+++ Lesson1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lesson1
+{
+	public static void main(String[] args)
+	{
+		String name;
+		int age;
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+		age = sc.nextInt();
+		System.out.println("name " + name);
+		System.out.println("age "+ age);
+	}
+}
